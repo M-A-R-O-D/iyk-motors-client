b152a7f6c0ea00fc16c986709b09c820de4199d8
Adehwam21 <drewaaron96@gmail.com>
2024-07-21 03:54:31 +0000

    feat: add homepage component

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import HeroSection from './HeroSection/HeroSection.jsx';
+import RentalSection from './RentalSection/RentalSection.jsx';
+import AboutSection from './AboutSection/AboutSection.jsx';
+import BrandSection from './BrandSection/BrandSection.jsx';
+import Footer from '../../components/Footer.jsx';
 
 function Homepage() {
-  return (
-    <div>Homepage</div>
-  )
+    return (
+        <div className="flex flex-col min-h-screen">
+            <div className="flex-grow">
+                <HeroSection />
+            </div>
+            <div className="flex-grow">
+                <RentalSection />
+            </div>
+            <div className="flex-grow">
+                <AboutSection />
+            </div>
+            <div className="flex-grow">
+                <BrandSection />
+            </div>
+            <div className="flex-grow">
+                <Footer />
+            </div>
+        </div>
+    );
 }
 
-export default Homepage
+export default Homepage;
