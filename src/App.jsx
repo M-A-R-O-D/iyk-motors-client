16ec98fa2a84c4d9ce0a3b32a4a320ebf85806c7
coderman2 <okquaye@st.ug.edu.gh>
2024-07-22 19:36:41 +0000

    progress

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-     <nav className='Navbar w-full bg-white h-140px flex flex-row items-center justify-between px-2.5 py-2.5 fixed'>
+     <nav className='Navbar w-full bg-white h-[70px]  flex flex-row items-center justify-between px-2.5 py-2.5 fixed z-10'>
       <div className='logo'>
       <span className='font-semibold text-[32px] mr-1.5'>IY-KAY</span>
       <span className='font-semibold text-[32px] text-[#1e7ed7]'>MOTORS</span>
@@ -24,99 +24,275 @@ function App() {
       </div>
      </nav>
 
-     <section className='w-full h-[600px] '>
-
+     <section className='relative w-full h-[600px] bg-cover bg-center pt-[70px]' style={{ backgroundImage: "url('/assets/nissan T3.jpg')" }}>
+        <div className='p-4 '>
+        <p className='bebas-neue text-white text-[56px]'>NISSAN T3</p>
+        <div className="BTNS flex">
+          <button className='bg-[#1e7ed7] w-auto h-7 bebas-neue p-1 m-2 text-white'>LEARN MORE</button>
+          <button className='bg-white bebas-neue w-auto h-7 p-1 m-2'>TEST DRIVE</button>
+        </div>
+        </div>
      </section>
 
      <section className='w-full flex flex-col justify-center p-4'>
         <div className="brands-text text-center">
           <div className="top-text flex flex-row items-center justify-center">
             <hr className='h-1 w-[66.5px] border-none bg-[#999] mr-[15px]' />
-            <p className='text-[#999] text-[35px]'>BRANDS</p>
+            <p className='text-[#999] text-[35px] bebas-neue'>BRANDS</p>
             <hr className='h-1 w-[66.5px] border-none bg-[#999] ml-[15px]' />
           </div>
         </div>
 
         <div className='text-center'>
-          <p className='text-[black] text-[54px]'>DISCOVER BY <span className='text-[#1e7ed7]'>BRANDS</span></p>
+          <p className='text-[black] text-[54px] bebas-neue'>DISCOVER BY <span className='text-[#1e7ed7]'>BRANDS</span></p>
         </div>
 
-        <div className="brand-logos flex flex-row flex-wrap justify-evenly">
-          <div className="logo-1 border border-black w-[200px] h-[84px] rounded-lg bg-[#f3f3f3] items-center justify-center">
-            <img src="/assets/BMW.png" alt="BMW logo" className="max-w-full max-h-full" />
+        <div className="brand-logos flex flex-row flex-wrap justify-evenly mb-14">
+          <div className="logo-1 border m-2 border-black w-[200px] h-[84px] rounded-lg bg-[#f3f3f3] flex items-center justify-center">
+            <img src="/assets/bm-logo.png" alt="BMW logo" className="max-w-full max-h-full" />
           </div>
-          <div className="logo-1 border border-black w-[200px] h-[84px] rounded-lg bg-[#f3f3f3] ">
+          <div className="logo-1 border m-2 border-black w-[200px] h-[84px] rounded-lg bg-[#f3f3f3] flex items-center justify-center">
             <img src="/assets/Honda.png" alt="BMW logo" className="max-w-full max-h-full"/>
           </div>
-          <div className="logo-1 border border-black w-[200px] h-[84px] rounded-lg bg-[#f3f3f3] ">
-            <img src="/assets/Porsche.jpg" alt="BMW logo" className="max-w-full max-h-full" />
+          <div className="logo-1 border m-2 border-black w-[200px] h-[84px] rounded-lg bg-[#f3f3f3] flex items-center justify-center">
+            <img src="/assets/Porsche.png" alt="BMW logo" className="max-w-full max-h-full" />
           </div>
-          <div className="logo-1 border border-black w-[200px] h-[84px] rounded-lg bg-[#f3f3f3] ">
-            <img src="/assets/Toyota.jpg" alt="BMW logo" className="max-w-full max-h-full" />
+          <div className="logo-1 border m-2 border-black w-[200px] h-[84px] rounded-lg bg-[#f3f3f3] flex items-center justify-center ">
+            <img src="/assets/t-logo.png" alt="BMW logo" className="max-w-full max-h-full" />
           </div>
-          <div className="logo-1 border border-black w-[200px] h-[84px] rounded-lg bg-[#f3f3f3] ">
+          <div className="logo-1 border m-2 border-black w-[200px] h-[84px] rounded-lg bg-[#f3f3f3] flex items-center justify-center">
             <img src="/assets/Mercedes.png" alt="BMW logo" className="max-w-full max-h-full" />
           </div>
           
-          <div className="logo-1 border border-black w-[200px] h-[84px] rounded-lg bg-[#f3f3f3] ">
-          <img src="/assets/chevrolet.jpg" alt="BMW logo" className="max-w-full max-h-full" />
+          <div className="logo-1 border m-2 border-black w-[200px] h-[84px] rounded-lg bg-[#f3f3f3] flex items-center justify-center">
+          <img src="/assets/chevy-logo.png" alt="BMW logo" className="max-w-full max-h-full" />
           </div>
         </div>
 
         <div className="brands-text text-center">
           <div className="top-text flex flex-row items-center justify-center">
             <hr className='h-1 w-[66.5px] border-none bg-[#999] mr-[15px]' />
-            <p className='text-[#999] text-[35px]'>STYLE</p>
+            <p className='text-[#999] text-[35px] bebas-neue'>STYLE</p>
             <hr className='h-1 w-[66.5px] border-none bg-[#999] ml-[15px]' />
           </div>
         </div>
 
         <div className='text-center'>
-          <p className='text-[black] text-[54px]'>DISCOVER BY <span className='text-[#1e7ed7]'>STYLE</span></p>
+          <p className='text-[black] text-[54px] bebas-neue'>DISCOVER BY <span className='text-[#1e7ed7]'>STYLE</span></p>
         </div>
 
-        <div className="cars flex flex-row flex-wrap justify-evenly">
-        <div className="logo-1 border border-black w-[120px] h-[120px] rounded-lg bg-[#f3f3f3] ">
-          <img src="/assets/chevrolet.jpg" alt="BMW logo" className="max-w-full max-h-full" />
+        <div className="cars flex flex-row flex-wrap justify-evenly mb-14">
+        <div className="logo-1 border m-4 border-black w-[120px] h-[120px] rounded-lg bg-[#f3f3f3] flex flex-col items-center justify-center ">
+          <img src="/assets/sport-car-icon-0.png" alt="BMW logo" className="max-w-full h-[75px]" />
+          <div>
+            <p className='bebas-neue text-center'>sports</p>
+            <p className='text-[12px] text-gray-500'>230 cars available</p>
           </div>
-        <div className="logo-1 border border-black w-[120px] h-[120px] rounded-lg bg-[#f3f3f3] ">
-          <img src="/assets/chevrolet.jpg" alt="BMW logo" className="max-w-full max-h-full" />
           </div>
-        <div className="logo-1 border border-black w-[120px] h-[120px] rounded-lg bg-[#f3f3f3] ">
-          <img src="/assets/chevrolet.jpg" alt="BMW logo" className="max-w-full max-h-full" />
+          <div className="logo-1 border m-4 border-black w-[120px] h-[120px] rounded-lg bg-[#f3f3f3] flex flex-col items-center justify-center ">
+          <img src="/assets/suv.png" alt="BMW logo" className="max-w-full h-[75px]" />
+          <div>
+            <p className='bebas-neue text-center'>suv</p>
+            <p className='text-[12px] text-gray-500'>230 cars available</p>
           </div>
-        <div className="logo-1 border border-black w-[120px] h-[120px] rounded-lg bg-[#f3f3f3] ">
-          <img src="/assets/chevrolet.jpg" alt="BMW logo" className="max-w-full max-h-full" />
           </div>
-        <div className="logo-1 border border-black w-[120px] h-[120px] rounded-lg bg-[#f3f3f3] ">
-          <img src="/assets/chevrolet.jpg" alt="BMW logo" className="max-w-full max-h-full" />
+          <div className="logo-1 border m-4 border-black w-[120px] h-[120px] rounded-lg bg-[#f3f3f3] flex flex-col items-center justify-center ">
+          <img src="/assets/convertible.png" alt="BMW logo" className="max-w-full h-[75px]" />
+          <div>
+            <p className='bebas-neue text-center'>convertible</p>
+            <p className='text-[12px] text-gray-500'>230 cars available</p>
           </div>
-        <div className="logo-1 border border-black w-[120px] h-[120px] rounded-lg bg-[#f3f3f3] ">
-          <img src="/assets/chevrolet.jpg" alt="BMW logo" className="max-w-full max-h-full" />
           </div>
-        <div className="logo-1 border border-black w-[120px] h-[120px] rounded-lg bg-[#f3f3f3] ">
-          <img src="/assets/chevrolet.jpg" alt="BMW logo" className="max-w-full max-h-full" />
+          <div className="logo-1 border m-4 border-black w-[120px] h-[120px] rounded-lg bg-[#f3f3f3] flex flex-col items-center justify-center ">
+          <img src="/assets/Luxury.png" alt="BMW logo" className="max-w-full h-[75px]" />
+          <div>
+            <p className='bebas-neue text-center'>Luxury</p>
+            <p className='text-[12px] text-gray-500'>230 cars available</p>
           </div>
-        <div className="logo-1 border border-black w-[120px] h-[120px] rounded-lg bg-[#f3f3f3] ">
-          <img src="/assets/chevrolet.jpg" alt="BMW logo" className="max-w-full max-h-full" />
+          </div>
+          <div className="logo-1 border m-4 border-black w-[120px] h-[120px] rounded-lg bg-[#f3f3f3] flex flex-col items-center justify-center ">
+          <img src="/assets/coupe.png" alt="BMW logo" className="max-w-full h-[75px]" />
+          <div>
+            <p className='bebas-neue text-center'>coupe</p>
+            <p className='text-[12px] text-gray-500'>230 cars available</p>
+          </div>
+          </div>
+          <div className="logo-1 border m-4 border-black w-[120px] h-[120px] rounded-lg bg-[#f3f3f3] flex flex-col items-center justify-center ">
+          <img src="/assets/EXR.png" alt="BMW logo" className="max-w-full h-[75px]" />
+          <div>
+            <p className='bebas-neue text-center'>exr model</p>
+            <p className='text-[12px] text-gray-500'>230 cars available</p>
+          </div>
+          </div>
+          <div className="logo-1 border m-4 border-black w-[120px] h-[120px] rounded-lg bg-[#f3f3f3] flex flex-col items-center justify-center ">
+          <img src="/assets/mercedes-model.png" alt="BMW logo" className="max-w-full h-[75px]" />
+          <div>
+            <p className='bebas-neue text-center'>Mercedes model</p>
+            <p className='text-[12px] text-gray-500'>230 cars available</p>
+          </div>
+          </div>
+          <div className="logo-1 border m-4 border-black w-[120px] h-[120px] rounded-lg bg-[#f3f3f3] flex flex-col items-center justify-center ">
+          <img src="/assets/BMW model.png" alt="BMW logo" className="max-w-full h-[75px]" />
+          <div>
+            <p className='bebas-neue text-center'>BMW MODEL</p>
+            <p className='text-[12px] text-gray-500'>230 cars available</p>
+          </div>
           </div>
         </div>
 
         <div className="brands-text text-center">
           <div className="top-text flex flex-row items-center justify-center">
             <hr className='h-1 w-[66.5px] border-none bg-[#999] mr-[15px]' />
-            <p className='text-[#999] text-[35px]'>CARS</p>
+            <p className='text-[#999] text-[35px] bebas-neue'>CARS</p>
             <hr className='h-1 w-[66.5px] border-none bg-[#999] ml-[15px]' />
           </div>
         </div>
 
         <div className='text-center'>
-          <p className='text-[black] text-[54px]'>SEARCH FOR <span className='text-[#1e7ed7]'>CARS</span></p>
+          <p className='text-[black] text-[54px] bebas-neue'>SEARCH FOR <span className='text-[#1e7ed7]'>CARS</span></p>
         </div>
-
+           
+  <div className="search-bars flex ">
+    <div className="relative flex items-center border-[1px] p-4 m-4 border-black rounded-xl w-96 h-14">
+      <input
+        type="text"
+        placeholder="VEHICLE NAME OR BRAND"
+        className="w-full h-full pl-4 pr-10 border-none outline-none rounded-xl"
+      />
+      <span className="material-symbols-outlined absolute right-4 text-gray-400">keyboard_arrow_down</span>
+    </div>
+    <div className="relative flex items-center border-[1px] p-4 m-4 border-black rounded-xl w-96 h-14">
+      <input
+        type="text"
+        placeholder="MODEL"
+        className="w-full h-full pl-4 pr-10 border-none outline-none rounded-xl"
+      />
+      <span className="material-symbols-outlined absolute right-4 text-gray-400">keyboard_arrow_down</span>
+    </div>        
+    <div className="relative flex items-center border-[1px] p-4 m-4 border-black rounded-xl w-56 h-14">
+      <input
+        type="text"
+        placeholder="PRICE UP TO"
+        className="w-full h-full pl-4 pr-10 border-none outline-none rounded-xl"
+      />
+      <span className="material-symbols-outlined absolute right-4 text-gray-400">keyboard_arrow_down</span>
+    </div>
+    <button className='btn w-48 h-14 m-4 bg-[#0e5a9f] text-white bebas-neue'>SEARCH</button>
+  </div>
       </section>
 
-    </>
+      <section>
+        <div className="1 p-8">
+          <p className='text-[26px] bebas-neue'>FILTER-03</p>
+        </div>
+        <div className="2 flex flex-row ">
+          <div className=" sidebar w-60  p-5 flex-shrink-0 ">
+          
+            <p className="bebas-neue text-gray-500 text-[26px]">
+              VEHICLE TYPE
+            </p>
+            {/* <span className="material-symbols-outlined absolute right-0 top-1/2 transform -translate-y-1/2 text-gray-400">
+              keyboard_arrow_down
+            </span> */}
+         
+            <hr />
+            <p><span>&#9723;</span>Economy</p>
+            <p><span>&#9723;</span>Compact</p>
+            <p><span>&#9723;</span>Midsize</p>
+            <p><span>&#9723;</span>Standard</p>
+            <p><span>&#9723;</span>Full size</p>
+            <p><span>&#9723;</span>Sports utility</p>
+            <p><span>&#9723;</span>Sports car</p>
+            
+            <p className='bebas-neue'>TRANSMISSION TYPE</p>
+            <hr />
+            <p className='bebas-neue'>CITY</p>
+            <hr />
+            <p className='bebas-neue'>PAYMENT METHOD</p>
+            <hr />
+            <p className='bebas-neue'>NUMBER OF SEATS</p>
+            <hr />
+            <p className='bebas-neue'>KILOMETER GAUGE</p>
+          </div>
+
+          <div className='car-blog '>
+            <p className='bebas-neue text-[26px]'>WE HAVE FOUND <span className='text-[#1e7ed7]'>365 CARS </span>FOR YOU</p>
+             <div className='flex flex-wrap'>
+            <div className='w-[320px] h-[400px] flex flex-col justify-center items-center ml-3 mr-3 mb-5'>
+             <img className='w-full' src="/assets/nissan nav.png" alt="Nissan navara" />
+             <h3 className='bebas-neue text-[26px]'>NISSAN NAVARA <span className='text-[#1e7ed7]'>- 2023</span></h3>
+             <p className='mb-8 '>start prices from GHc 490,000</p>
+             <button className='w-72 h-10 bg-[#0e5a9f] text-white bebas-neue' >PROCEED TO BUY</button>
+          </div>
+          <div className='w-[320px] h-[400px] flex flex-col justify-center items-center ml-3 mr-3 mb-5'>
+             <img className='w-full' src="/assets/XE.jpg" alt="Nissan navara" />
+             <h3 className='bebas-neue text-[26px]'>NISSAN NAVARA XE <span className='text-[#1e7ed7]'> + - 2024</span></h3>
+             <p className='mb-8 '>start prices from GHc 502,425</p>
+             <button className='w-72 h-10 bg-[#0e5a9f] text-white bebas-neue' >PROCEED TO BUY</button>
+          </div>
+          <div className='w-[320px] h-[400px] flex flex-col justify-center items-center ml-3 mr-3 mb-5 '>
+             <img className='w-full h-48' src="/assets/Thigh.jpg" alt="Nissan navara" />
+             <h3 className='bebas-neue text-[26px]'>2023 TOYOTA <span className='text-[#1e7ed7]'>HIGHLANDER</span></h3>
+             <p className='mb-8 '>start prices from $ 10,000</p>
+             <button className='w-72 h-10 bg-[#0e5a9f] text-white bebas-neue' >PROCEED TO BUY</button>
+          </div>
+          <div className='w-[320px] h-[400px] flex flex-col justify-center items-center ml-3 mr-3 mb-5'>
+             <img className='w-full' src="/assets/thundra.jpg" alt="Nissan navara" />
+             <h3 className='bebas-neue text-[26px]'>TOYOTA THUNDRA <span className='text-[#1e7ed7]'>- 2022</span></h3>
+             <p className='mb-8 '>start prices from GHc 2,346,250</p>
+             <button className='w-72 h-10 bg-[#0e5a9f] text-white bebas-neue' >PROCEED TO BUY</button>
+          </div>
+          <div className='w-[320px] h-[400px] flex flex-col justify-center items-center ml-3 mr-3 mb-5'>
+             <img className='w-full' src="/assets/bmw.jpg" alt="Nissan navara" />
+             <h3 className='bebas-neue text-[26px]'>BMW 330I<span className='text-[#1e7ed7]'>- 2020</span></h3>
+             <p className='mb-8 '>start prices from GHc 688,750</p>
+             <button className='w-72 h-10 bg-[#0e5a9f] text-white bebas-neue' >PROCEED TO BUY</button>
+          </div>
+          <div className='w-[320px] h-[400px] flex flex-col justify-center items-center ml-3 mr-3 mb-5'>
+             <img className='w-full h-48' src="/assets/h21.jpg" alt="Nissan navara" />
+             <h3 className='bebas-neue text-[26px]'>HONDA CR-V<span className='text-[#1e7ed7]'>- 2021</span></h3>
+             <p className='mb-8 '>start prices from GHc 430,000</p>
+             <button className='w-72 h-10 bg-[#0e5a9f] text-white bebas-neue' >PROCEED TO BUY</button>
+          </div>
+          
+          </div>
+          
+          </div>
+          
+        </div>
+      </section>
+
+      <section>
+      <footer className='w-full h-auto bg-[rgb(14,90,159)] flex flex-col items-center'>
+    <div className='flex items-center justify-center'>
+        <p className='font-bold text-[35px] text-white mt-8 '>IY-KAY MOTORS</p>
+    </div>
+    <div className='w-full flex justify-center'>
+        <ul className='flex justify-between'>
+            <li className='mr-9'>
+                <a href="" className='font-semibold text-[15px] mr-9 text-white decoration-2 decoration-[#1e7ed7] hover:underline' style={{ textUnderlineOffset: '15px' }}>HOME</a>
+                <a href="" className='font-semibold text-[15px] mr-9 text-white decoration-2 decoration-[#1e7ed7] hover:underline' style={{ textUnderlineOffset: '15px' }}>CARS</a>
+                <a href="" className='font-semibold text-[15px] mr-9 text-white decoration-2 decoration-[#1e7ed7] hover:underline' style={{ textUnderlineOffset: '15px' }}>ABOUT US</a>
+                <a href="" className='font-semibold text-[15px] mr-9 text-white decoration-2 decoration-[#1e7ed7] hover:underline' style={{ textUnderlineOffset: '15px' }}>OUR BLOG</a>
+            </li>
+        </ul>
+    </div>
+      <hr className='m-[10.5px] w-[1080px]'/>
+      <div className='ml-auto flex'>
+       <div className='m-8'>
+       <p className='font-semibold text-white'>CONTACT US:</p>
+        <p className='text-white'>+233 59 8285 85</p>
+        <p className='text-white'>Achimota-Mile 7(opposite fidelity bank)</p>
+       </div>
+
+        <div className="socials m-8" >
+          <p className='font-semibold text-white'>CONNECT WITH US</p>
+        </div>
+      </div>
+        </footer>
+      </section>
+
+    </> 
   )
 }
 
