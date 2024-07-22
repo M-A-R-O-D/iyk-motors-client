c276d83f900a5aa042304babb5715045bd711d6d
Adehwam21 <drewaaron96@gmail.com>
2024-07-22 03:33:30 +0000

    feat: update company about

--- src/pages/Homepage/AboutSection/AboutSection.jsx
+++ src/pages/Homepage/AboutSection/AboutSection.jsx
@@ -1,24 +1,31 @@
 import React from 'react';
 import Button from '../../../components/Button';
+import { Icon } from '@iconify-icon/react';
+import AnimatedSection from '../../../components/Animator';
 
 function AboutSection() {
     return (
         <div className="bg-cover bg-center flex flex-col justify-end items-end p-16 text-white font-poppins"
             style={{ backgroundImage: `url('/assets/images/Rectangle27.png')` }}>
-            <div className='h-100 mt-16 pt-5 pr-10'>
-                <div className="max-w-md">
-                    <h1 className="text-5xl font-bold mb-4">
-                        THE BEST IN THE <span className="text-custom-blue">AUTOMOBILE</span> INDUSTRY
-                    </h1>
-                    <p className="mb-6">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Reprehenderit aliquam facilis iure molestiae doloribus cumque laborum incidunt,
-                        officia consectetur quasi alias atque suscipit, eius, nesciunt repellat corrupti!
-                        Reprehenderit impedit debitis cum commodi laborum voluptates.
-                    </p>
-                    <Button children={"Read more"} className="border border-white text-white bg-transparent hover:bg-white hover:text-black px-4 py-2 rounded transition duration-300" />
+            <AnimatedSection>
+                <div className='h-100 mt-16 mr-14 pt-20 pr-10'>
+                    <div className="max-w-md">
+                        <h1 className="text-4xl font-bold mb-4">
+                            THE BEST IN THE <span className="text-custom-blue">AUTOMOBILE</span> INDUSTRY
+                        </h1>
+                        <p className="mb-6">
+                            At IYK Motors, we pride ourselves on being a leader in the automobile industry.
+                            Our commitment to excellence, innovation, and customer satisfaction sets us apart.
+                            With a diverse range of high-quality vehicles, competitive pricing,
+                            and a team of experienced professionals, we ensure you drive away with confidence and satisfaction.
+                        </p>
+                        <Button className="bg-transparent h-12 w-40 flex justify-center items-center  space-x-2 border border-white text-white hover:bg-white hover:text-black">
+                            <span>Read more</span>
+                            <Icon icon="ri:arrow-right-line" style={{ fontSize: '24px' }} />
+                        </Button>
+                    </div>
                 </div>
-            </div>
+            </AnimatedSection>
         </div>
     );
 }
