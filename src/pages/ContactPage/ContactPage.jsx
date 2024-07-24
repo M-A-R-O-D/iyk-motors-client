18e26dc75023c145bb7b589bd9574c3a15b152ce
saintdannyyy <dnaddo004@st.ug.edu.gh>
2024-07-24 18:01:05 +0000

    Added Hero and Footer to Contact page

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import HeroSection from './Hero/Hero.jsx';
+import Footer from '../../components/Footer.jsx';
 
 function ContactPage() {
     return (
-        <div>To be developed...</div>
-    )
+        <div className="flex flex-col min-h-screen">
+            <div className="flex-grow">
+                <HeroSection />
+            </div>
+            <div className="flex-grow">
+                <Footer />
+            </div>
+        </div>
+    );
 }
 
-export default ContactPage
+export default ContactPage;
