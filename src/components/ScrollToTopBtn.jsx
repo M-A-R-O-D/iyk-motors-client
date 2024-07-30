895b5b8a7b86e0ddc6e129f533748de9cfc546cb
Adehwam21 <drewaaron96@gmail.com>
2024-07-30 04:47:47 +0000

    feat: add scroll to top button to pages

---
+++ src/components/ScrollToTopBtn.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import Button from './Button'; // Adjust the path as necessary
+import { Icon } from '@iconify/react';
+
+const ScrollToTopBtn = ({ onClick }) => {
+    const [isVisible, setIsVisible] = useState(false);
+
+    const toggleVisibility = () => {
+        if (window.scrollY > 250) {
+            setIsVisible(true);
+        } else {
+            setIsVisible(false);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', toggleVisibility);
+        return () => window.removeEventListener('scroll', toggleVisibility);
+    }, []);
+
+    return (
+        <Button
+            onClick={onClick}
+            className={`bg-custom-white border-2 rounded-1 border-custom-blue text-custom-blue fixed bottom-10 right-[-3px] transform transition-transform duration-300 ${isVisible ? 'translate-x-0' : 'translate-x-full'
+                }`}
+        >
+            <Icon icon="ph:caret-double-up-bold" style={{ fontSize: '24px' }} />
+        </Button>
+    );
+};
+
+export default ScrollToTopBtn;
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import HeroSection from './HeroSection/HeroSection.jsx';
 import RentalSection from './RentalSection/RentalSection.jsx';
 import AboutSection from './AboutSection/AboutSection.jsx';
-import ServiceSection from './ServiceSection/ServiceSection.jsx';
 import FeaturedVehicles from './VehicleSection/FeaturedVehicles.jsx';
 import Footer from '../../components/Footer.jsx';
 import FeaturedBrands from './FeaturedBrands.jsx/FeaturedBrands.jsx';
+import ScrollToTopBtn from '../../components/ScrollToTopBtn.jsx';
 
 function Homepage() {
     return (
-        <div className="flex flex-col min-h-screen">
-            <div className="flex-grow">
+        <div className="flex flex-col min-h-screen relative">
+            <div id='home-hero-section' className="flex-grow">
                 <HeroSection />
             </div>
             <div className="flex-grow">
@@ -28,6 +28,7 @@ function Homepage() {
             <div className="flex-grow">
                 <Footer />
             </div>
+            <ScrollToTopBtn onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} />
         </div>
     );
 }
